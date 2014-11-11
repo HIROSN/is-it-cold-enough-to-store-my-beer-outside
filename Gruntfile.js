228aa178a1c49093085baad53383d87e7844d8db
Hiroshi Nagata <hirosn@live.com>
2014-11-10 23:12:47 -0800

    Reset hard.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function(grunt) {
+  var srcFiles = ['*.js', 'public/**/*.js'];
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  grunt.initConfig({
+    jshint: {
+      files: srcFiles,
+      options: {
+        jshintrc: true
+      }
+    },
+
+    jscs: {
+      src: srcFiles,
+      options: {
+        preset: 'google'
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['jshint', 'jscs']);
+  grunt.registerTask('default',  ['test']);
+};
