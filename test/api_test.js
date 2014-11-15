c3e4cfe65ea41094b290aadcf9d6088feeff5175
Hiroshi Nagata <hirosn@live.com>
2014-11-15 15:39:49 -0800

    Add some tests. It still might not detect the client side parse error in jsonp request.

--- test/api_test.js
+++ test/api_test.js
@@ -22,6 +22,32 @@ describe('REST API tests', function() {
     });
   });
 
+  it('should return 500 for post /api without data', function(done) {
+    chai.request(server).
+    post('/api').
+    end(function(err, res) {
+      expect(err).equals(null);
+      expect(res).to.be.a('object');
+      expect(res).to.have.status(500);
+      done();
+    });
+  });
+
+  it('should work for post /api with Google Public DNS IP', function(done) {
+    chai.request(server).
+    post('/api').
+    send({ip: '8.8.8.8'}).
+    end(function(err, res) {
+      expect(err).equals(null);
+      expect(res).to.be.a('object');
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.a('object');
+      expect(res.body.isp).to.equal('Google');
+      expect(res.body.city).to.equal('Mountain View, CA USA');
+      done();
+    });
+  });
+
   it('should return 500 for get /api without query', function(done) {
     chai.request(server).
     get('/api').
@@ -29,6 +55,20 @@ describe('REST API tests', function() {
       expect(err).equals(null);
       expect(res).to.be.a('object');
       expect(res).to.have.status(500);
+      done();
+    });
+  });
+
+  it('should work for get /api with Google Public DNS IP', function(done) {
+    chai.request(server).
+    get('/api?ip=8.8.8.8').
+    end(function(err, res) {
+      expect(err).equals(null);
+      expect(res).to.be.a('object');
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.a('object');
+      expect(res.body.isp).to.equal('Google');
+      expect(res.body.city).to.equal('Mountain View, CA USA');
       done();
     });
   });
